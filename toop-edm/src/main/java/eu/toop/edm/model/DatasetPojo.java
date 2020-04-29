acba96edd53915e601efde958450b833c302a708
Philip Helger <philip@helger.com>
2020-04-29 17:38:53 +0200

    Guess what: Schema update

---
+++ toop-edm/src/main/java/eu/toop/edm/model/DatasetPojo.java
@@ -0,0 +1,172 @@
+package eu.toop.edm.model;
+
+import java.time.LocalDateTime;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.collection.impl.CommonsArrayList;
+import com.helger.commons.collection.impl.ICommonsList;
+import com.helger.commons.string.StringHelper;
+
+import eu.toop.edm.jaxb.dcatap.DCatAPDatasetType;
+
+public class DatasetPojo
+{
+  private final String m_sID;
+  private final ICommonsList <String> m_aTitles = new CommonsArrayList <> ();
+  private final ICommonsList <String> m_aDescriptions = new CommonsArrayList <> ();
+  private final LocalDateTime m_aIssuedDT;
+  private final LocalDateTime m_aLastModifiedDT;
+  private final String m_sLanguage;
+  private final ICommonsList <String> m_aErrorCodes;
+
+  public DatasetPojo (@Nullable final String sID,
+                      @Nonnull @Nonempty final ICommonsList <String> aTitles,
+                      @Nonnull @Nonempty final ICommonsList <String> aDescriptions,
+                      @Nullable final LocalDateTime aIssuedDT,
+                      @Nullable final LocalDateTime aLastModifiedDT,
+                      @Nullable final String sLanguage,
+                      @Nullable final ICommonsList <String> aErrorCodes)
+  {
+    ValueEnforcer.notEmptyNoNullValue (aTitles, "Titles");
+    ValueEnforcer.notEmptyNoNullValue (aDescriptions, "Descriptions");
+
+    m_sID = sID;
+    m_aTitles.addAll (aTitles);
+    m_aDescriptions.addAll (aDescriptions);
+    m_aIssuedDT = aIssuedDT;
+    m_aLastModifiedDT = aLastModifiedDT;
+    m_sLanguage = sLanguage;
+    m_aErrorCodes = aErrorCodes;
+  }
+
+  @Nonnull
+  public DCatAPDatasetType getAsDataset ()
+  {
+    final DCatAPDatasetType ret = new DCatAPDatasetType ();
+    if (StringHelper.hasText (m_sID))
+      ret.addContent (new eu.toop.edm.jaxb.dcterms.ObjectFactory ().createIdentifier (m_sID));
+    for (final String sTitle : m_aTitles)
+      ret.addContent (new eu.toop.edm.jaxb.dcterms.ObjectFactory ().createTitle (sTitle));
+    for (final String sDescription : m_aDescriptions)
+      ret.addContent (new eu.toop.edm.jaxb.dcterms.ObjectFactory ().createDescription (sDescription));
+    return ret;
+  }
+
+  @Nonnull
+  public static Builder builder ()
+  {
+    return new Builder ();
+  }
+
+  public static class Builder
+  {
+    private String m_sID;
+    private final ICommonsList <String> m_aTitles = new CommonsArrayList <> ();
+    private final ICommonsList <String> m_aDescriptions = new CommonsArrayList <> ();
+    private LocalDateTime m_aIssuedDT;
+    private LocalDateTime m_aLastModifiedDT;
+    private String m_sLanguage;
+    private ICommonsList <String> m_aErrorCodes;
+
+    public Builder ()
+    {}
+
+    @Nonnull
+    public Builder id (@Nullable final String s)
+    {
+      m_sID = s;
+      return this;
+    }
+
+    @Nonnull
+    public Builder addTitle (@Nullable final String s)
+    {
+      if (StringHelper.hasText (s))
+        m_aTitles.add (s);
+      return this;
+    }
+
+    @Nonnull
+    public Builder title (@Nullable final String s)
+    {
+      if (StringHelper.hasText (s))
+        m_aTitles.set (s);
+      else
+        m_aTitles.clear ();
+      return this;
+    }
+
+    @Nonnull
+    public Builder addDescription (@Nullable final String s)
+    {
+      if (StringHelper.hasText (s))
+        m_aDescriptions.add (s);
+      return this;
+    }
+
+    @Nonnull
+    public Builder description (@Nullable final String s)
+    {
+      if (StringHelper.hasText (s))
+        m_aDescriptions.set (s);
+      else
+        m_aDescriptions.clear ();
+      return this;
+    }
+
+    @Nonnull
+    public Builder issued (@Nullable final LocalDateTime a)
+    {
+      m_aIssuedDT = a;
+      return this;
+    }
+
+    @Nonnull
+    public Builder lastModified (@Nullable final LocalDateTime a)
+    {
+      m_aLastModifiedDT = a;
+      return this;
+    }
+
+    @Nonnull
+    public Builder language (@Nullable final String s)
+    {
+      m_sLanguage = s;
+      return this;
+    }
+
+    @Nonnull
+    public Builder addErrorCode (@Nullable final String s)
+    {
+      if (StringHelper.hasText (s))
+        m_aErrorCodes.add (s);
+      return this;
+    }
+
+    @Nonnull
+    public Builder errorCode (@Nullable final String s)
+    {
+      if (StringHelper.hasText (s))
+        m_aErrorCodes.set (s);
+      else
+        m_aErrorCodes.clear ();
+      return this;
+    }
+
+    @Nonnull
+    public DatasetPojo build ()
+    {
+      return new DatasetPojo (m_sID,
+                              m_aTitles,
+                              m_aDescriptions,
+                              m_aIssuedDT,
+                              m_aLastModifiedDT,
+                              m_sLanguage,
+                              m_aErrorCodes);
+    }
+  }
+}
---
+++ toop-edm/src/test/java/eu/toop/edm/model/DatasetPojoTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2018-2020 toop.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.toop.edm.model;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.toop.edm.jaxb.dcatap.DCatAPDatasetType;
+import eu.toop.edm.xml.dcatap.DatasetMarshaller;
+
+/**
+ * Test class for class {@link DatasetPojo}
+ *
+ * @author Philip Helger
+ */
+public final class DatasetPojoTest
+{
+  private static final Logger LOGGER = LoggerFactory.getLogger (DatasetPojoTest.class);
+
+  @Test
+  public void testBasic ()
+  {
+    final DatasetPojo x = DatasetPojo.builder ().build ();
+    final DCatAPDatasetType aAgent = x.getAsDataset ();
+    assertNotNull (aAgent);
+
+    final DatasetMarshaller m = new DatasetMarshaller ();
+    m.setFormattedOutput (true);
+    assertNotNull (m.getAsDocument (aAgent));
+    LOGGER.info (m.getAsString (aAgent));
+  }
+
+  @Test
+  public void testMinimum ()
+  {
+    final DatasetPojo x = DatasetPojo.builder ().build ();
+    assertNotNull (x);
+
+    final DCatAPDatasetType aAgent = x.getAsDataset ();
+    assertNotNull (aAgent);
+
+    final DatasetMarshaller m = new DatasetMarshaller ();
+    m.setFormattedOutput (true);
+    assertNotNull (m.getAsDocument (aAgent));
+    LOGGER.info (m.getAsString (aAgent));
+  }
+}
