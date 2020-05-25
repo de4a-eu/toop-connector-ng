13abe30e4de73abf99b49f2f038c1091eea5aea8
Philip Helger <philip@helger.com>
2020-05-25 09:02:07 +0200

    Updated license headers

--- tc-api/src/main/java/eu/toop/connector/api/TCConfig.java
+++ tc-api/src/main/java/eu/toop/connector/api/TCConfig.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2018-2020 toop.eu
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,25 +43,28 @@ import eu.toop.connector.api.me.EMEProtocol;
  * @author Philip Helger
  */
 @ThreadSafe
-public final class TCConfig {
-  private static final SimpleReadWriteLock s_aRWLock = new SimpleReadWriteLock();
+public final class TCConfig
+{
+  private static final SimpleReadWriteLock s_aRWLock = new SimpleReadWriteLock ();
 
-  @GuardedBy("s_aRWLock")
+  @GuardedBy ("s_aRWLock")
   private static IConfig s_aConfig;
 
-  static {
-    setDefaultConfig();
+  static
+  {
+    setDefaultConfig ();
   }
 
-  private TCConfig() {
-  }
+  private TCConfig ()
+  {}
 
   /**
    * @return The configuration file. Never <code>null</code>.
    */
   @Nonnull
-  public static IConfig getConfig() {
-    return s_aRWLock.readLockedGet(() -> s_aConfig);
+  public static IConfig getConfig ()
+  {
+    return s_aRWLock.readLockedGet ( () -> s_aConfig);
   }
 
   /**
@@ -70,33 +73,39 @@ public final class TCConfig {
    * @param aConfig
    *        The config to be set. May not be <code>null</code>.
    */
-  public static void setConfig(@Nonnull final IConfig aConfig) {
-    ValueEnforcer.notNull(aConfig, "Config");
-    s_aRWLock.writeLockedGet(() -> s_aConfig = aConfig);
+  public static void setConfig (@Nonnull final IConfig aConfig)
+  {
+    ValueEnforcer.notNull (aConfig, "Config");
+    s_aRWLock.writeLockedGet ( () -> s_aConfig = aConfig);
   }
 
   /**
    * Set the default configuration.
    */
-  public static void setDefaultConfig() {
-    setConfig(ConfigFactory.getDefaultConfig());
+  public static void setDefaultConfig ()
+  {
+    setConfig (ConfigFactory.getDefaultConfig ());
   }
 
   @Nonnull
-  public static IIdentifierFactory getIdentifierFactory() {
+  public static IIdentifierFactory getIdentifierFactory ()
+  {
     return TCIdentifierFactory.INSTANCE_TC;
   }
 
-  public static class Global {
-    private Global() {
+  public static class Global
+  {
+    private Global ()
+    {}
+
+    public static boolean isGlobalDebug ()
+    {
+      return getConfig ().getAsBoolean ("global.debug", GlobalDebug.isDebugMode ());
     }
 
-    public static boolean isGlobalDebug() {
-      return getConfig().getAsBoolean("global.debug", GlobalDebug.isDebugMode());
-    }
-
-    public static boolean isGlobalProduction() {
-      return getConfig().getAsBoolean("global.production", GlobalDebug.isProductionMode());
+    public static boolean isGlobalProduction ()
+    {
+      return getConfig ().getAsBoolean ("global.production", GlobalDebug.isProductionMode ());
     }
 
     /**
@@ -104,54 +113,62 @@ public final class TCConfig {
      *         address is used.
      */
     @Nullable
-    public static String getToopInstanceName() {
-      return getConfig().getAsString("global.instancename");
+    public static String getToopInstanceName ()
+    {
+      return getConfig ().getAsString ("global.instancename");
     }
   }
 
-  public static class Tracker {
+  public static class Tracker
+  {
     public static final boolean DEFAULT_TOOP_TRACKER_ENABLED = false;
     public static final String DEFAULT_TOOP_TRACKER_TOPIC = "toop";
 
-    private Tracker() {
-    }
+    private Tracker ()
+    {}
 
-    public static boolean isToopTrackerEnabled() {
-      return getConfig().getAsBoolean("toop.tracker.enabled", DEFAULT_TOOP_TRACKER_ENABLED);
-    }
-
-    @Nullable
-    public static String getToopTrackerUrl() {
-      return getConfig().getAsString("toop.tracker.url");
+    public static boolean isToopTrackerEnabled ()
+    {
+      return getConfig ().getAsBoolean ("toop.tracker.enabled", DEFAULT_TOOP_TRACKER_ENABLED);
     }
 
     @Nullable
-    public static String getToopTrackerTopic() {
-      return getConfig().getAsString("toop.tracker.topic", DEFAULT_TOOP_TRACKER_TOPIC);
+    public static String getToopTrackerUrl ()
+    {
+      return getConfig ().getAsString ("toop.tracker.url");
+    }
+
+    @Nullable
+    public static String getToopTrackerTopic ()
+    {
+      return getConfig ().getAsString ("toop.tracker.topic", DEFAULT_TOOP_TRACKER_TOPIC);
     }
   }
 
-  public static class DSD {
-    private DSD() {
-    }
+  public static class DSD
+  {
+    private DSD ()
+    {}
 
     /**
      * @return The TOOP Directory base URL for R2D2. Should never end with a
      *         slash.
      */
     @Nullable
-    public static String getR2D2DirectoryBaseUrl() {
-      return getConfig().getAsString("toop.dsd.service.baseurl");
+    public static String getR2D2DirectoryBaseUrl ()
+    {
+      return getConfig ().getAsString ("toop.dsd.service.baseurl");
     }
 
   }
 
-  public static class R2D2 {
+  public static class R2D2
+  {
     public static final boolean DEFAULT_USE_SML = true;
     private static ISMLInfo s_aCachedSMLInfo;
 
-    private R2D2() {
-    }
+    private R2D2 ()
+    {}
 
     /**
      * @return <code>true</code> to use SML lookup, <code>false</code> to not do
@@ -159,8 +176,9 @@ public final class TCConfig {
      * @see #getR2D2SML()
      * @see #getR2D2SMPUrl()
      */
-    public static boolean isR2D2UseDNS() {
-      return getConfig().getAsBoolean("toop.r2d2.usedns", DEFAULT_USE_SML);
+    public static boolean isR2D2UseDNS ()
+    {
+      return getConfig ().getAsBoolean ("toop.r2d2.usedns", DEFAULT_USE_SML);
     }
 
     /**
@@ -168,24 +186,29 @@ public final class TCConfig {
      *         {@link #isR2D2UseDNS()} returned <code>true</code>.
      */
     @Nonnull
-    public static ISMLInfo getR2D2SML() {
+    public static ISMLInfo getR2D2SML ()
+    {
       ISMLInfo ret = s_aCachedSMLInfo;
-      if (ret == null) {
-        final String sSMLID = getConfig().getAsString("toop.r2d2.sml.id");
-        final ESML eSML = ESML.getFromIDOrNull(sSMLID);
-        if (eSML != null) {
+      if (ret == null)
+      {
+        final String sSMLID = getConfig ().getAsString ("toop.r2d2.sml.id");
+        final ESML eSML = ESML.getFromIDOrNull (sSMLID);
+        if (eSML != null)
+        {
           // Pre-configured SML it is
           ret = eSML;
-        } else {
+        }
+        else
+        {
           // Custom SML
-          final String sDisplayName = getConfig().getAsString("toop.r2d2.sml.name", "TOOP SML");
+          final String sDisplayName = getConfig ().getAsString ("toop.r2d2.sml.name", "TOOP SML");
           // E.g. edelivery.tech.ec.europa.eu.
-          final String sDNSZone = getConfig().getAsString("toop.r2d2.sml.dnszone");
+          final String sDNSZone = getConfig ().getAsString ("toop.r2d2.sml.dnszone");
           // E.g. https://edelivery.tech.ec.europa.eu/edelivery-sml
-          final String sManagementServiceURL = getConfig().getAsString("toop.r2d2.sml.serviceurl");
-          final boolean bClientCertificateRequired = getConfig().getAsBoolean("toop.r2d2.sml.clientcert", false);
+          final String sManagementServiceURL = getConfig ().getAsString ("toop.r2d2.sml.serviceurl");
+          final boolean bClientCertificateRequired = getConfig ().getAsBoolean ("toop.r2d2.sml.clientcert", false);
           // No need for a persistent ID here
-          ret = new SMLInfo(GlobalIDFactory.getNewStringID(), sDisplayName, sDNSZone, sManagementServiceURL, bClientCertificateRequired);
+          ret = new SMLInfo (GlobalIDFactory.getNewStringID (), sDisplayName, sDNSZone, sManagementServiceURL, bClientCertificateRequired);
         }
         // Remember in cache
         s_aCachedSMLInfo = ret;
@@ -198,24 +221,27 @@ public final class TCConfig {
      *         {@link #isR2D2UseDNS()} returned <code>false</code>.
      */
     @Nullable
-    public static URI getR2D2SMPUrl() {
+    public static URI getR2D2SMPUrl ()
+    {
       // E.g. http://smp.central.toop
-      final String sURI = getConfig().getAsString("toop.r2d2.smp.url");
-      return URLHelper.getAsURI(sURI);
+      final String sURI = getConfig ().getAsString ("toop.r2d2.smp.url");
+      return URLHelper.getAsURI (sURI);
     }
   }
 
-  public static class MEM {
-    private MEM() {
-    }
+  public static class MEM
+  {
+    private MEM ()
+    {}
 
     /**
      * @return The MEM implementation ID or the default value. Never
      *         <code>null</code>.
      */
     @Nullable
-    public static String getMEMImplementationID() {
-      return getConfig().getAsString("toop.mem.implementation");
+    public static String getMEMImplementationID ()
+    {
+      return getConfig ().getAsString ("toop.mem.implementation");
     }
 
     /**
@@ -225,88 +251,104 @@ public final class TCConfig {
      * @return The overall protocol to use. Never <code>null</code>.
      */
     @Nonnull
-    public static EMEProtocol getMEMProtocol() {
-      final String sID = getConfig().getAsString("toop.mem.protocol", EMEProtocol.DEFAULT.getID());
-      final EMEProtocol eProtocol = EMEProtocol.getFromIDOrNull(sID);
+    public static EMEProtocol getMEMProtocol ()
+    {
+      final String sID = getConfig ().getAsString ("toop.mem.protocol", EMEProtocol.DEFAULT.getID ());
+      final EMEProtocol eProtocol = EMEProtocol.getFromIDOrNull (sID);
       if (eProtocol == null)
-        throw new IllegalStateException("Failed to resolve protocol with ID '" + sID + "'");
+        throw new IllegalStateException ("Failed to resolve protocol with ID '" + sID + "'");
       return eProtocol;
     }
 
     // GW_URL
     @Nullable
-    public static String getMEMAS4Endpoint() {
-      return getConfig().getAsString("toop.mem.as4.endpoint");
+    public static String getMEMAS4Endpoint ()
+    {
+      return getConfig ().getAsString ("toop.mem.as4.endpoint");
     }
 
     @Nullable
-    public static String getMEMAS4GwPartyID() {
-      return getConfig().getAsString("toop.mem.as4.gw.partyid");
+    public static String getMEMAS4GwPartyID ()
+    {
+      return getConfig ().getAsString ("toop.mem.as4.gw.partyid");
     }
 
     @Nullable
-    public static String getMEMAS4TcPartyid() {
-      return getConfig().getAsString("toop.mem.as4.tc.partyid");
+    public static String getMEMAS4TcPartyid ()
+    {
+      return getConfig ().getAsString ("toop.mem.as4.tc.partyid");
     }
 
-    public static long getGatewayNotificationWaitTimeout() {
-      return getConfig().getAsLong("toop.mem.as4.notificationWaitTimeout", 20000);
+    public static long getGatewayNotificationWaitTimeout ()
+    {
+      return getConfig ().getAsLong ("toop.mem.as4.notificationWaitTimeout", 20000);
     }
 
     /**
-     * @return The URL where incoming AS4 messages are forwarded to
+     * @return The URL where incoming AS4 messages are forwarded to. This is the
+     *         value from the configuration file.
      */
     @Nullable
-    public static String getMEMIncomingURL() {
-      return getConfig().getAsString("toop.mem.incoming.url");
+    public static String getMEMIncomingURL ()
+    {
+      return getConfig ().getAsString ("toop.mem.incoming.url");
     }
-
   }
 
-  public static class HTTP {
-    private HTTP() {
-    }
+  public static class HTTP
+  {
+    private HTTP ()
+    {}
 
-    public static boolean isProxyServerEnabled() {
-      return getConfig().getAsBoolean("http.proxy.enabled", false);
+    public static boolean isProxyServerEnabled ()
+    {
+      return getConfig ().getAsBoolean ("http.proxy.enabled", false);
     }
 
     @Nullable
-    public static String getProxyServerAddress() {
+    public static String getProxyServerAddress ()
+    {
       // Scheme plus hostname or IP address
-      return getConfig().getAsString("http.proxy.address");
+      return getConfig ().getAsString ("http.proxy.address");
     }
 
     @CheckForSigned
-    public static int getProxyServerPort() {
-      return getConfig().getAsInt("http.proxy.port", -1);
+    public static int getProxyServerPort ()
+    {
+      return getConfig ().getAsInt ("http.proxy.port", -1);
     }
 
     @Nullable
-    public static String getProxyServerNonProxyHosts() {
+    public static String getProxyServerNonProxyHosts ()
+    {
       // Separated by pipe
-      return getConfig().getAsString("http.proxy.non-proxy");
+      return getConfig ().getAsString ("http.proxy.non-proxy");
     }
 
-    public static boolean isTLSTrustAll() {
-      return getConfig().getAsBoolean("http.tls.trustall", false);
+    public static boolean isTLSTrustAll ()
+    {
+      return getConfig ().getAsBoolean ("http.tls.trustall", false);
     }
 
-    public static int getConnectionTimeoutMS() {
-      return getConfig().getAsInt("http.connection-timeout", -1);
+    public static int getConnectionTimeoutMS ()
+    {
+      return getConfig ().getAsInt ("http.connection-timeout", -1);
     }
 
-    public static int getReadTimeoutMS() {
-      return getConfig().getAsInt("http.read-timeout", -1);
+    public static int getReadTimeoutMS ()
+    {
+      return getConfig ().getAsInt ("http.read-timeout", -1);
     }
   }
 
-  public static class WebApp {
-    private WebApp() {
-    }
+  public static class WebApp
+  {
+    private WebApp ()
+    {}
 
-    public static boolean isStatusEnabled() {
-      return getConfig().getAsBoolean("toop.webapp.status.enabled", true);
+    public static boolean isStatusEnabled ()
+    {
+      return getConfig ().getAsBoolean ("toop.webapp.status.enabled", true);
     }
   }
 }
--- tc-main/src/main/java/eu/toop/connector/app/incoming/MPTrigger.java
+++ tc-main/src/main/java/eu/toop/connector/app/incoming/MPTrigger.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2018-2020 toop.eu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.toop.connector.app.incoming;
 
 import javax.annotation.Nonnull;
@@ -8,6 +23,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 
 import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.Nonempty;
 import com.helger.commons.collection.ArrayHelper;
 import com.helger.commons.error.level.EErrorLevel;
 import com.helger.commons.mime.IMimeType;
@@ -39,103 +55,118 @@ import eu.toop.regrep.CRegRep4;
  * @author Philip Helger
  */
 @Immutable
-public final class MPTrigger {
-  private MPTrigger() {
-  }
+public final class MPTrigger
+{
+  private MPTrigger ()
+  {}
 
   @Nonnull
-  private static ESuccess _forwardMessage(@Nonnull final TCIncomingMessage aMsg, final String sDestURL) {
-    ValueEnforcer.notNull(aMsg, "Msg");
-    ValueEnforcer.notNull(sDestURL, "Destination URL");
+  private static ESuccess _forwardMessage (@Nonnull final TCIncomingMessage aMsg, @Nonnull @Nonempty final String sDestURL)
+  {
+    ValueEnforcer.notNull (aMsg, "Msg");
+    ValueEnforcer.notEmpty (sDestURL, "Destination URL");
 
-    if (StringHelper.hasNoText(sDestURL))
-      throw new IllegalStateException("No URL for handling inbound messages is defined.");
-    final IURLProtocol aProtocol = URLProtocolRegistry.getInstance().getProtocol(sDestURL);
+    if (StringHelper.hasNoText (sDestURL))
+      throw new IllegalStateException ("No URL for handling inbound messages is defined.");
+    final IURLProtocol aProtocol = URLProtocolRegistry.getInstance ().getProtocol (sDestURL);
     if (aProtocol == null)
-      throw new IllegalStateException("The URL for handling inbound messages is invalid.");
+      throw new IllegalStateException ("The URL for handling inbound messages is invalid.");
 
     // Convert XML to bytes
-    final byte[] aPayload = TCRestJAXB.incomingMessage().getAsBytes(aMsg);
+    final byte [] aPayload = TCRestJAXB.incomingMessage ().getAsBytes (aMsg);
     if (aPayload == null)
-      throw new IllegalStateException();
+      throw new IllegalStateException ();
 
-    ToopKafkaClient.send(EErrorLevel.INFO, () -> "Sending inbound message to '" + sDestURL + "' with " + aPayload.length + " bytes");
+    ToopKafkaClient.send (EErrorLevel.INFO, () -> "Sending inbound message to '" + sDestURL + "' with " + aPayload.length + " bytes");
 
     // Main sending
-    try (final HttpClientManager aHCM = HttpClientManager.create(new TCHttpClientSettings())) {
-      final HttpPost aPost = new HttpPost(sDestURL);
-      aPost.setEntity(new ByteArrayEntity(aPayload));
-      final byte[] aResult = aHCM.execute(aPost, new ResponseHandlerByteArray());
+    try (final HttpClientManager aHCM = HttpClientManager.create (new TCHttpClientSettings ()))
+    {
+      final HttpPost aPost = new HttpPost (sDestURL);
+      aPost.setEntity (new ByteArrayEntity (aPayload));
+      final byte [] aResult = aHCM.execute (aPost, new ResponseHandlerByteArray ());
 
-      ToopKafkaClient.send(EErrorLevel.INFO,
-          () -> "Sending inbound message was successful. Got " + ArrayHelper.getSize(aResult) + " bytes back");
+      ToopKafkaClient.send (EErrorLevel.INFO,
+                            () -> "Sending inbound message was successful. Got " + ArrayHelper.getSize (aResult) + " bytes back");
       return ESuccess.SUCCESS;
-    } catch (final Exception ex) {
-      ToopKafkaClient.send(EErrorLevel.ERROR, () -> "Sending inbound message to '" + sDestURL + "' failed", ex);
+    }
+    catch (final Exception ex)
+    {
+      ToopKafkaClient.send (EErrorLevel.ERROR, () -> "Sending inbound message to '" + sDestURL + "' failed", ex);
       return ESuccess.FAILURE;
     }
   }
 
   @Nonnull
-  private static TCIncomingMetadata _createMetadata(@Nonnull final IMEIncomingTransportMetadata aMD,
-                                                    @Nonnull final TCPayloadType ePayloadType) {
-    final TCIncomingMetadata ret = new TCIncomingMetadata();
-    ret.setSenderID(TCRestJAXB.createTCID(aMD.getSenderID()));
-    ret.setReceiverID(TCRestJAXB.createTCID(aMD.getReceiverID()));
-    ret.setDocTypeID(TCRestJAXB.createTCID(aMD.getDocumentTypeID()));
-    ret.setProcessID(TCRestJAXB.createTCID(aMD.getProcessID()));
-    ret.setPayloadType(ePayloadType);
+  private static TCIncomingMetadata _createMetadata (@Nonnull final IMEIncomingTransportMetadata aMD,
+                                                     @Nonnull final TCPayloadType ePayloadType)
+  {
+    final TCIncomingMetadata ret = new TCIncomingMetadata ();
+    ret.setSenderID (TCRestJAXB.createTCID (aMD.getSenderID ()));
+    ret.setReceiverID (TCRestJAXB.createTCID (aMD.getReceiverID ()));
+    ret.setDocTypeID (TCRestJAXB.createTCID (aMD.getDocumentTypeID ()));
+    ret.setProcessID (TCRestJAXB.createTCID (aMD.getProcessID ()));
+    ret.setPayloadType (ePayloadType);
     return ret;
   }
 
   @Nonnull
-  private static TCPayload _createPayload(@Nonnull final byte[] aValue,
-                                          @Nullable final String sContentID,
-                                          @Nonnull final IMimeType aMimeType) {
-    final TCPayload ret = new TCPayload();
-    ret.setValue(aValue);
-    ret.setContentID(sContentID);
-    ret.setMimeType(aMimeType.getAsString());
+  private static TCPayload _createPayload (@Nonnull final byte [] aValue,
+                                           @Nullable final String sContentID,
+                                           @Nonnull final IMimeType aMimeType)
+  {
+    final TCPayload ret = new TCPayload ();
+    ret.setValue (aValue);
+    ret.setContentID (sContentID);
+    ret.setMimeType (aMimeType.getAsString ());
     return ret;
   }
 
   @Nonnull
-  public static ESuccess forwardMessage(@Nonnull final IncomingEDMRequest aRequest) {
-    return forwardMessage(aRequest, TCConfig.MEM.getMEMIncomingURL());
+  public static ESuccess forwardMessage (@Nonnull final IncomingEDMRequest aRequest)
+  {
+    return forwardMessage (aRequest, TCConfig.MEM.getMEMIncomingURL ());
   }
 
   @Nonnull
-  public static ESuccess forwardMessage(@Nonnull final IncomingEDMRequest aRequest, String sDestUrl) {
-    final TCIncomingMessage aMsg = new TCIncomingMessage();
-    aMsg.setMetadata(_createMetadata(aRequest.getMetadata(), TCPayloadType.REQUEST));
-    aMsg.addPayload(_createPayload(aRequest.getRequest().getWriter().getAsBytes(), null, CRegRep4.MIME_TYPE_EBRS_XML));
-    return _forwardMessage(aMsg, sDestUrl);
+  public static ESuccess forwardMessage (@Nonnull final IncomingEDMRequest aRequest, @Nonnull @Nonempty final String sDestUrl)
+  {
+    final TCIncomingMessage aMsg = new TCIncomingMessage ();
+    aMsg.setMetadata (_createMetadata (aRequest.getMetadata (), TCPayloadType.REQUEST));
+    aMsg.addPayload (_createPayload (aRequest.getRequest ().getWriter ().getAsBytes (), null, CRegRep4.MIME_TYPE_EBRS_XML));
+    return _forwardMessage (aMsg, sDestUrl);
   }
 
   @Nonnull
-  public static ESuccess forwardMessage(@Nonnull final IncomingEDMResponse aResponse) {
-    return forwardMessage(aResponse, TCConfig.MEM.getMEMIncomingURL());
+  public static ESuccess forwardMessage (@Nonnull final IncomingEDMResponse aResponse)
+  {
+    return forwardMessage (aResponse, TCConfig.MEM.getMEMIncomingURL ());
   }
+
   @Nonnull
-  public static ESuccess forwardMessage(@Nonnull final IncomingEDMResponse aResponse, String sDestURL) {
-    final TCIncomingMessage aMsg = new TCIncomingMessage();
-    aMsg.setMetadata(_createMetadata(aResponse.getMetadata(), TCPayloadType.RESPONSE));
-    aMsg.addPayload(_createPayload(aResponse.getResponse().getWriter().getAsBytes(), null, CRegRep4.MIME_TYPE_EBRS_XML));
+  public static ESuccess forwardMessage (@Nonnull final IncomingEDMResponse aResponse, @Nonnull @Nonempty final String sDestURL)
+  {
+    final TCIncomingMessage aMsg = new TCIncomingMessage ();
+    aMsg.setMetadata (_createMetadata (aResponse.getMetadata (), TCPayloadType.RESPONSE));
+    aMsg.addPayload (_createPayload (aResponse.getResponse ().getWriter ().getAsBytes (), null, CRegRep4.MIME_TYPE_EBRS_XML));
     // Add all attachments
-    for (final MEPayload aPayload : aResponse.attachments().values())
-      aMsg.addPayload(_createPayload(aPayload.getData().bytes(), aPayload.getContentID(), aPayload.getMimeType()));
-    return _forwardMessage(aMsg, sDestURL);
+    for (final MEPayload aPayload : aResponse.attachments ().values ())
+      aMsg.addPayload (_createPayload (aPayload.getData ().bytes (), aPayload.getContentID (), aPayload.getMimeType ()));
+    return _forwardMessage (aMsg, sDestURL);
   }
 
   @Nonnull
-  public static ESuccess forwardMessage(@Nonnull final IncomingEDMErrorResponse aErrorResponse) {
-    return forwardMessage(aErrorResponse, TCConfig.MEM.getMEMIncomingURL());
+  public static ESuccess forwardMessage (@Nonnull final IncomingEDMErrorResponse aErrorResponse)
+  {
+    return forwardMessage (aErrorResponse, TCConfig.MEM.getMEMIncomingURL ());
   }
+
   @Nonnull
-  public static ESuccess forwardMessage(@Nonnull final IncomingEDMErrorResponse aErrorResponse, String sDestURL) {
-    final TCIncomingMessage aMsg = new TCIncomingMessage();
-    aMsg.setMetadata(_createMetadata(aErrorResponse.getMetadata(), TCPayloadType.ERROR_RESPONSE));
-    aMsg.addPayload(_createPayload(aErrorResponse.getErrorResponse().getWriter().getAsBytes(), null, CRegRep4.MIME_TYPE_EBRS_XML));
-    return _forwardMessage(aMsg, sDestURL);
+  public static ESuccess forwardMessage (@Nonnull final IncomingEDMErrorResponse aErrorResponse, @Nonnull @Nonempty final String sDestURL)
+  {
+    final TCIncomingMessage aMsg = new TCIncomingMessage ();
+    aMsg.setMetadata (_createMetadata (aErrorResponse.getMetadata (), TCPayloadType.ERROR_RESPONSE));
+    aMsg.addPayload (_createPayload (aErrorResponse.getErrorResponse ().getWriter ().getAsBytes (), null, CRegRep4.MIME_TYPE_EBRS_XML));
+    return _forwardMessage (aMsg, sDestURL);
   }
 }
